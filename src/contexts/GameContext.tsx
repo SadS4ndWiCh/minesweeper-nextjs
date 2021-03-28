bc878c8e4b0d44ec8e553b68740810c19c67027f
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-28 20:56:46 -0300

    add: game context functions

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,96 +1,100 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-import { countNeighbors, createBoard } from "@utils/board";
+import { difficulties } from '../config';
+
+type Difficulty = 'easy' | 'medium' | 'hard'; 
 
 interface IGameConfigurations {
   width: number;
-  heigth: number;
-
+  height: number;
   totalBombs: number;
+  difficulty: Difficulty;
 }
 
 interface IBoardCell {
   isRevealed: boolean;
   neighborsCount: number;
-  
-  isFlagged?: boolean;
-  isBomb?: boolean;
-}
-
-interface GameContextData {
-  configurations: IGameConfigurations,
-  boardCells: IBoardCell[],
-  isGameRunning: boolean;
-
-  init: () => void;
-  setBoardCells: Dispatch<SetStateAction<IBoardCell[]>>;
-  remainingBombsCount: () => number;
-  remainingFlagsCount: () => number;
-  start: () => void;
-  finish: () => void;
-  reset: () => void;
+  isFlagged: boolean;
+  isBomb: boolean;
 }
 
 interface GameProviderProps {
   children: ReactNode;
 }
 
+interface GameContextData {
+  configurations: IGameConfigurations,
+  isGameOver: boolean;
+  hasClear: boolean;
+
+  changeDifficulty: (difficulty: Difficulty) => void;
+  isClear: (board: IBoardCell[]) => boolean;
+  finish: (hasWon: boolean) => void;
+  resetGame: () => void;
+}
+
 export const GameContext = createContext({} as GameContextData);
 
 export function GameProvider({ children }: GameProviderProps) {
   const [configurations, setConfigurations] = useState<IGameConfigurations>({
-    width: 10,
-    heigth: 10,
-    totalBombs: 15,
+    width: difficulties.easy.width,
+    height: difficulties.easy.height,
+    totalBombs: difficulties.easy.totalBombs,
+    difficulty: 'easy'
   });
-  const [boardCells, setBoardCells] = useState<IBoardCell[]>(undefined);
-  const [isGameRunning, setIsGameRunning] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(undefined);
+  const [hasClear, setHasClear] = useState(undefined);
 
-  function init() {
-    let board = createBoard(configurations.width, configurations.heigth, configurations.totalBombs);
-    board = countNeighbors(board, configurations); 
-    setBoardCells(board);
+  // Mudar a dificuldade do jogo
+  function changeDifficulty(difficulty: Difficulty) {
+    const newConfigurations: IGameConfigurations = {
+      width: difficulties[difficulty].width,
+      height: difficulties[difficulty].height,
+      totalBombs: difficulties[difficulty].totalBombs,
+      difficulty,
+    };
+
+    setConfigurations(newConfigurations);
   }
 
-  function start() {
-    setIsGameRunning(true);
+  // Checar se ganhou o jogo
+  function isClear(board: IBoardCell[]) {
+    // Ganhar o jogo seria revelar todos os elementos exeto os que contém bomba,
+    // ou seja, para checar se já ganhou ou não, é ver se a quantidade de 
+    // elementos revelados é igual a quantidade de bombas, pois assim, apenas
+    // as bombas que terão sobrado
+    let totalClear = board.reduce((total, curr) => curr.isRevealed ? total + 1 : total, 0);
+
+    return totalClear === ( configurations.width * configurations.height - configurations.totalBombs )
   }
 
-  function finish() {
-    setIsGameRunning(false);
+  // Define se venceu ou não
+  function finish(hasWon: boolean) {
+    if(hasWon) {
+      setHasClear(true);
+      setIsGameOver(false);
+    } else {
+      setHasClear(false);
+      setIsGameOver(true);
+    }
   }
 
-  function reset() {
-    finish();
-    init();
-  }
-
-  function remainingBombsCount() {
-    const board: IBoardCell[] = JSON.parse(JSON.stringify(boardCells));
-    const remainingBombs = board.filter(cell => cell.isBomb && !cell.isFlagged);
-
-    return remainingBombs.length
-  }
-
-  function remainingFlagsCount() {
-    const board: IBoardCell[] = JSON.parse(JSON.stringify(boardCells));
-    const remainingFlags = board.filter(cell => cell.isFlagged);
-
-    return configurations.totalBombs - remainingFlags.length
+  // Reseta o jogo
+  function resetGame() {
+    setHasClear(undefined);
+    setIsGameOver(undefined);
   }
 
   return (
     <GameContext.Provider value={{
       configurations,
-      boardCells,
-      isGameRunning,
-      init,
-      setBoardCells,
-      remainingBombsCount,
-      remainingFlagsCount,
-      start,
+      isGameOver,
+      hasClear,
+      
+      changeDifficulty,
+      isClear,
       finish,
-      reset,
+      resetGame,
     }}>
       { children }
     </GameContext.Provider>
