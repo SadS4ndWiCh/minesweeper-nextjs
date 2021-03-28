b914d215c10186ae1d1e3107a207646c450cb18f
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-28 20:55:00 -0300

    add: use Game component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { Board } from '@components/Board'
-
-import { GameProvider } from '@contexts/GameContext'
+import { Game } from '@components/Game'
 
 import styles from '@styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <GameProvider>
-        <Board />
-      </GameProvider>
+      <main>
+        <Game />
+      </main>
     </div>
   )
 }
