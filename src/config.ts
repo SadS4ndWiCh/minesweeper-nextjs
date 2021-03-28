40369a4d9bf8df591da48d25fcc8ef4942b892a7
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-28 20:51:13 -0300

    add: game difficulty configurations

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export const difficulties = {
+  easy: {
+    width: 9,
+    height: 9,
+    totalBombs: 10,
+  },
+  medium: {
+    width: 16,
+    height: 16,
+    totalBombs: 40,
+  },
+  hard: {
+    width: 30,
+    height: 16,
+    totalBombs: 99,
+  },
+}
