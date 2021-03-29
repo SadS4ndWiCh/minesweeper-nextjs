503274112496f8442660f7f79657cd58c983f8d2
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-28 21:01:19 -0300

    add: RemainingFlags component

--- src/components/RemainingFlags.tsx
+++ src/components/RemainingFlags.tsx
@@ -4,14 +4,29 @@ import { GameContext } from '@contexts/GameContext';
 
 import styles from '@styles/components/RemainingFlags.module.css';
 
-export function RemainingFlags() {
-  const { boardCells, configurations, remainingFlagsCount } = useContext(GameContext);
-  
+interface IBoardCell {
+  isRevealed: boolean;
+  neighborsCount: number;
+  isFlagged: boolean;
+  isBomb: boolean;
+}
+
+interface RemainingFlagsProps {
+  board: IBoardCell[];
+}
+
+export function RemainingFlags({ board }: RemainingFlagsProps) {
+  const { configurations } = useContext(GameContext);
   const [remainingFlags, setRemainingFlags] = useState(configurations.totalBombs);
 
   useEffect(() => {
-    if(boardCells) setRemainingFlags(remainingFlagsCount());
-  }, [boardCells])
+    if(board) {
+      const totalFlags = board.reduce((total, currentCell) => currentCell.isFlagged ? total + 1 : total, 0);
+  
+      setRemainingFlags(configurations.totalBombs - totalFlags);
+    }
+
+  }, [board]);
 
   return (
     <div className={`${styles.remainingFlagsContainer} neu-flat`}>
