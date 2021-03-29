21d623e319efc683c6d1bc27517655c7c5b7b8b9
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-28 22:19:32 -0300

    add: Head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { Game } from '@components/Game'
 
 import styles from '@styles/pages/Home.module.css'
@@ -5,6 +7,10 @@ import styles from '@styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Minesweeper</title>
+      </Head>
+
       <main>
         <Game />
       </main>
