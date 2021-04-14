ecde4938e5dcbb264e51d60ded561dab6c165854
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-04-14 19:58:40 -0300

    refactor: change to useReducer

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,100 +1,28 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, Dispatch, ReactNode, useReducer } from "react";
 
-import { difficulties } from '../config';
-
-type Difficulty = 'easy' | 'medium' | 'hard'; 
-
-interface IGameConfigurations {
-  width: number;
-  height: number;
-  totalBombs: number;
-  difficulty: Difficulty;
-}
-
-interface IBoardCell {
-  isRevealed: boolean;
-  neighborsCount: number;
-  isFlagged: boolean;
-  isBomb: boolean;
-}
+import { gameReducer, initialGameState } from '../utils/game';
+import { IGameState } from '../utils/interfaces';
 
 interface GameProviderProps {
   children: ReactNode;
 }
 
 interface GameContextData {
-  configurations: IGameConfigurations,
-  isGameOver: boolean;
-  hasClear: boolean;
+  gameState: IGameState;
 
-  changeDifficulty: (difficulty: Difficulty) => void;
-  isClear: (board: IBoardCell[]) => boolean;
-  finish: (hasWon: boolean) => void;
-  resetGame: () => void;
+  gameDispatch: Dispatch<any>;
 }
 
 export const GameContext = createContext({} as GameContextData);
 
 export function GameProvider({ children }: GameProviderProps) {
-  const [configurations, setConfigurations] = useState<IGameConfigurations>({
-    width: difficulties.easy.width,
-    height: difficulties.easy.height,
-    totalBombs: difficulties.easy.totalBombs,
-    difficulty: 'easy'
-  });
-  const [isGameOver, setIsGameOver] = useState(undefined);
-  const [hasClear, setHasClear] = useState(undefined);
-
-  // Mudar a dificuldade do jogo
-  function changeDifficulty(difficulty: Difficulty) {
-    const newConfigurations: IGameConfigurations = {
-      width: difficulties[difficulty].width,
-      height: difficulties[difficulty].height,
-      totalBombs: difficulties[difficulty].totalBombs,
-      difficulty,
-    };
-
-    setConfigurations(newConfigurations);
-  }
-
-  // Checar se ganhou o jogo
-  function isClear(board: IBoardCell[]) {
-    // Ganhar o jogo seria revelar todos os elementos exeto os que contém bomba,
-    // ou seja, para checar se já ganhou ou não, é ver se a quantidade de 
-    // elementos revelados é igual a quantidade de bombas, pois assim, apenas
-    // as bombas que terão sobrado
-    let totalClear = board.reduce((total, curr) => curr.isRevealed ? total + 1 : total, 0);
-
-    return totalClear === ( configurations.width * configurations.height - configurations.totalBombs )
-  }
-
-  // Define se venceu ou não
-  function finish(hasWon: boolean) {
-    if(hasWon) {
-      setHasClear(true);
-      setIsGameOver(false);
-    } else {
-      setHasClear(false);
-      setIsGameOver(true);
-    }
-  }
-
-  // Reseta o jogo
-  function resetGame() {
-    setHasClear(undefined);
-    setIsGameOver(undefined);
-  }
-
+  const [gameState, gameDispatch] = useReducer(gameReducer, initialGameState);
+  
   return (
     <GameContext.Provider value={{
-      configurations,
-      isGameOver,
-      hasClear,
-      
-      changeDifficulty,
-      isClear,
-      finish,
-      resetGame,
+      gameState,
+  
+      gameDispatch
     }}>
       { children }
     </GameContext.Provider>
